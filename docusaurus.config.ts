dae6c08fb7ccf33c8f4215beaf25dfdc7f66c7c8
Vishnu Goswami <vishnugoswami6000@gmail.com>
2025-10-06 15:26:49 +0530

    initial

---
+++ docusaurus.config.ts
@@ -0,0 +1,110 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'Busiman - Business management',
+  tagline: 'All-in-one business management platform that helps you handle inventory, departments, and daily operations with real-time updates and notifications. ',
+  favicon: 'https://lh3.googleusercontent.com/p/AF1QipNdAaM1zzLbdDepo_pZKBHgKJZQKSd3QDXF4pOJ=s1360-w1360-h1020-rw',
+  future: {
+    v4: true,
+  },
+  url: 'https://docs.busiman.in',
+  baseUrl: '/',
+  organizationName: 'busiman',
+  projectName: 'busiman-docs',
+  onBrokenLinks: 'throw',
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: './sidebars.ts',
+          lastVersion: '1.0',
+          onlyIncludeVersions: ['1.0'],
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    colorMode: {
+      respectPrefersColorScheme: true,
+    },
+    navbar: {
+      title: 'Busiman',
+      logo: {
+        alt: 'Busiman Logo',
+        src: 'https://lh3.googleusercontent.com/p/AF1QipNdAaM1zzLbdDepo_pZKBHgKJZQKSd3QDXF4pOJ=s1360-w1360-h1020-rw',
+      },
+      items: [
+        { href:"https://busiman.in", label: 'Go to Busiman', position: 'right' },
+        {
+          href: 'https://github.com/facebook/docusaurus',
+          label: 'Contribute',
+          position: 'right',
+        },
+        {
+          type: 'docsVersionDropdown',
+        },
+      ],
+    },
+    // footer: {
+    //   style: 'dark',
+    //   links: [
+    //     {
+    //       title: 'Docs',
+    //       items: [
+    //         {
+    //           label: 'Tutorial',
+    //           to: '/',
+    //         },
+    //       ],
+    //     },
+    //     {
+    //       title: 'Community',
+    //       items: [
+    //         {
+    //           label: 'Stack Overflow',
+    //           href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+    //         },
+    //         {
+    //           label: 'Discord',
+    //           href: 'https://discordapp.com/invite/docusaurus',
+    //         },
+    //         {
+    //           label: 'X',
+    //           href: 'https://x.com/docusaurus',
+    //         },
+    //       ],
+    //     },
+    //     {
+    //       title: 'More',
+    //       items: [
+    //         {
+    //           label: 'Blog',
+    //           to: '/blog',
+    //         },
+    //         {
+    //           label: 'GitHub',
+    //           href: 'https://github.com/facebook/docusaurus',
+    //         },
+    //       ],
+    //     },
+    //   ],
+    //   copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    // },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
